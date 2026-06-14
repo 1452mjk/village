befc891f3a9f93e4f2370bbf8c16bfe9e9472e26
1452mjk <1315757950@qq.com>
2026-06-14 17:19:08 +0800

    Delete api directory

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-// src/api/index.ts
-export * from './auth'
-export * from './user'
-export * from './notice'
-export * from './affair'
-export * from './vote'
-export * from './product'
-export * from './service'
-export * from './statistics'
--- api/village.ts
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export const getVillageList = () => {
-  return request<Array<{ id: number; name: string }>>({
-    url: '/village/list',
-    method: 'get'
-  })
-}
